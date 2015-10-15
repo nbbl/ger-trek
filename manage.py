fcf76adb017b5cac8e05429ecdd4b1aea7755b8c
lePiv0C <lejay@physik.tu-berlin.de>
2015-10-15 18:18:59 +0200

    created manage script

---
+++ manage.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+from app import create_app, db
+from app.auth.models import User, Role, Datastore, users_roles
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role\
+            Datastore=Datastore)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+@manager.command
+def deploy(nick='admin', email='admin@app.de'):
+    db.create_all()
+    # create a superuser
+    import getpass
+    password = getpass.getpass()
+    u = User(nick=nick, email=email, password=password, \
+            '''role=admin''')
+    db.session.add(u)
+    db.session.commit()
+
+if __name__ == '__main__':
+    manager.run()

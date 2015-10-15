0f71a7e90d3b1a9c1c79b137afec537fa430cc66
lePiv0C <lejay@physik.tu-berlin.de>
2015-10-15 18:01:57 +0200

    reworked initialization of app into function and added Config Classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,36 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
-# Define the WSGI application object
-app = Flask(__name__)
-
-# Configurations
-app.config.from_object('default_config')
-# This envar should be set to the production config path
-# during the deployment process.
-# TODO: change to a less generic name, 
-# as soon as one has been found for this package.
-app.config.from_envvar('BLOG_APP_CONFIG', silent=True)
+from default_config import config
 
 
-# Define the database object which is imported
-# by modules and controllers
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# Import modules/components using their blueprint handler variables
-# TODO: try this at the beginning and see if it still works (circular deps?!)
-# from app.auth.controllers import auth as auth
-# ..
+def create_app(config_name):
+    # Define the WSGI application object
+    app = Flask(__name__)
+    
+    # Configurations
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app) 
+    
+    # Define the database object which is imported
+    # by modules and controllers
+    db.init_app(app)
 
-# Register blueprint(s)
-# app.register_blueprint(auth)
-# ..
+    return app
+    
+    # Import modules/components using their blueprint handler variables
+    # TODO: try this at the beginning and see if it still works (circular deps?!)
+    # from app.auth.controllers import auth as auth
+    # ..
+    
+    # Register blueprint(s)
+    # app.register_blueprint(auth)
+    # ..
 
 # Build the database:
-db.create_all()
+# This is being done in manage.py
+#db.create_all()
 # TODO: We also need a manager (from flask-script)
 # for setting up the database with persistent data (independent from the app-session),
 # e.g. creating admins, standard roles, etc.
